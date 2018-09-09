5161453b6f6b306786d1dd0b29a9c6580360cb44
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-09 15:33:20 +0200

    Add PE64 example

---
+++ helloworld64.asm
@@ -0,0 +1,24 @@
+format PE64 console
+entry main
+
+include 'win32a.inc'
+
+section '.text' code executable
+main:
+    push hello
+    cinvoke printf
+    cinvoke getchar
+    push 0
+    invoke ExitProcess
+
+section '.rdata' data readable
+    hello db 'Hello world!', 10, 0
+
+section '.idata' data readable import
+    library kernel32, 'kernel32.dll', \
+            msvcrt, 'msvcrt.dll'
+
+    import kernel32, ExitProcess, 'ExitProcess'
+    import msvcrt, \
+        printf, 'printf', \
+        getchar, 'getchar'
