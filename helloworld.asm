5faacacd87fa3ba65b29b292d6a4c8a2a644ec07
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-10 14:02:52 +0200

    Update formatting

--- helloworld.asm
+++ helloworld.asm
@@ -4,6 +4,7 @@ entry main
 include 'win32a.inc'
 
 section '.text' code executable
+
 main:
     push hello
     cinvoke printf
@@ -12,13 +13,17 @@ main:
     invoke ExitProcess
 
 section '.rdata' data readable
-    hello db 'Hello world!', 10, 0
+
+hello db 'Hello world!', 10, 0
 
 section '.idata' data readable import
-    library kernel32, 'kernel32.dll', \
-            msvcrt, 'msvcrt.dll'
 
-    import kernel32, ExitProcess, 'ExitProcess'
-    import msvcrt, \
-        printf, 'printf', \
-        getchar, 'getchar'
+library kernel32, 'kernel32.dll', \
+        msvcrt, 'msvcrt.dll'
+
+import kernel32, \
+       ExitProcess, 'ExitProcess'
+
+import msvcrt, \
+       printf, 'printf', \
+       getchar, 'getchar'
